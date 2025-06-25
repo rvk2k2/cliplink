affa4662c6a405b6240d0cadbcf0d1fbb3be2c56
rvk2k2 <rahulvk2002@gmail.com>
2025-06-25 23:05:31 +0530

    Implemented headers on authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
 const {getUser} = require('../service/auth')
 
 async function restrictToLoggedinUserOnly(req, res, next){
-     const userUid = req.cookies?.uid;
+     const userUid = req.headers["Autharization"];
     if(!userUid) return res.redirect('/login');
-    const user =  getUser(userUid);
+    const token = userUid.split("Bearer ")[1];
+    const user =  getUser(token);
+
     if(!user) return res.redirect("/login");
     req.user =  user;
     next();
 }
 
 async function checkAuth(req, res, next){
-    const userUid = req.cookies?.uid;
-    const user =  getUser(userUid);
-    req.user =  user;
+     const userUid = req.headers["autharization"];
+      const token = userUid.split("Bearer ")[1];
+      const user =  getUser(token);
+      req.user =  user;
     next();
 }
 
